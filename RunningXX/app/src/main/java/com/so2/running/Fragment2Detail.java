1d7fffac6214d8ba70e406b980b89791829caf69
chiuyanyu <s102213039@mail1.ncnu.edu.tw>
2016-08-08 14:48:50 +0800

    update

--- RunningXX/app/src/main/java/com/so2/running/Fragment2Detail.java
+++ RunningXX/app/src/main/java/com/so2/running/Fragment2Detail.java
@@ -22,16 +22,19 @@
 
 package com.so2.running;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Fragment2Detail extends Fragment {
     View view;
     Fragment2ListItem item;
+    Button returnbutton ;
 
     public void setItem (Fragment2ListItem item)
     {
@@ -42,8 +45,18 @@ public class Fragment2Detail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.frag2_detail, container, false);
+        returnbutton = (Button)view.findViewById(R.id.returnbutton);
 
 
+        returnbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FragmentTransaction ft = getFragmentManager().beginTransaction().replace(R.id.content_frame, new Fragment2());
+                ft.commit();
+            }
+        });
 
         //Get textview from view
         TextView name = (TextView) view.findViewById(R.id.name);
